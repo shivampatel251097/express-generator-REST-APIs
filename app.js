4f6d4a9d94f56405e928828fd3cf4d4d430aa203
Shivam Patel <shivampatel251097@gmail.com>
2021-01-08 00:50:48 +0530

    Cookies

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session =  require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -35,15 +37,22 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore
+}));
 
 //Basic Authentication starts here!!
 //Basiscally we authenticate before request comes to the middlwware responsible for routing
 function auth(req,res,next){
-  // console.log(req.headers);
-  console.log(req.signedCookies);
+  // console.log(req.signedCookies.user);
+  console.log(req.session);
 
-  if(!req.signedCookies.user){
+  if(!req.session.user){
       var authHeader =  req.headers.authorization;
 
       if(!authHeader){
@@ -58,7 +67,8 @@ function auth(req,res,next){
       var username =  auth[0];
       var password =  auth[1];
       if(username ==='admin' && password === 'password'){
-        res.cookie('user','admin',{signed:true})
+        // res.cookie('user','admin',{signed:true})
+        req.session.user = 'admin';
         next();
       }
       else{
@@ -69,7 +79,7 @@ function auth(req,res,next){
       }
   }
   else{
-    if(req.signedCookies.user ==='admin'){
+    if(req.session.user ==='admin'){
       next();
     }
     else{
