f0897e5ee17ae557deca151b0a42445721bd8440
Shivam Patel <shivampatel251097@gmail.com>
2021-01-17 23:36:23 +0530

    passport-auth

--- routes/users.js
+++ routes/users.js
@@ -16,25 +16,20 @@ router.get('/', function(req, res, next) {
 
 //route for sign up
 router.post('/signup', (req,res,next) => {
-User.findOne({username: req.body.username})
-.then((user)=>{
-  if(user !=null){
-    var err = new Error('User '+ req.body.username+ ' already exists');
-    err.status = 403;
-    next(err);
+User.register(new User({username: req.body.username}), req.body.password, (err,user)=>{
+  if(err){
+    res.statusCode = 500;
+    res.setHeader('Content-Type','application/json');
+    res.json({err: err});
   }
   else{
-    return User.create({
-      username: req.body.username,
-      password: req.body.password})
+    passport.authenticate('local')(req,res, ()=>{
+      res.statusCode = 200;
+      res.setHeader('Content-Type','application/json');
+      res.json({success: true, status: 'Registration Successful'});
+    });
   }
 })
-.then((user)=>{
-  res.statusCode = 200;
-  res.setHeader('Content-Type','application/json');
-  res.json({status: 'Registration Successful', user: user});
-}, (err) => next(err))
-.catch((err)=>next(err));
 });
 
 
